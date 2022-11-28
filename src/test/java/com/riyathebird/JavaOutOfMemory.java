2fde12429df921d9d9cc9b8dac092c2ad38871a4
Katja Sannikova <sannikova.k@gmail.com>
2022-11-29 02:42:16 +0300

    added boolean type

--- src/test/java/com/riyathebird/JavaOutOfMemory.java
+++ src/test/java/com/riyathebird/JavaOutOfMemory.java
@@ -5,16 +5,19 @@ public class JavaOutOfMemory {
 
         byte a = 127;
         short b = -32768;
-        int c = Integer.MAX_VALUE;
+        int c = 2147483647;
         long d = 5L;
         double e = 0.1d;
         float f = 11.25f;
 
+        boolean bool = a > b;
+        System.out.println(bool);
+
 // переполнение
 
-        System.out.println(a + 2);
+        System.out.println(a + 10);
         System.out.println(c);
-        System.out.println(c + 1);
+        System.out.println(c + 2);
         System.out.println(b - 1);
 
     }
